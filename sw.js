d3a9d6b4bff0da120a180e5e4573495c12f2f4e1
Yanik Segler <yanik.segler@gmail.com>
2026-09-09 16:58:56 +0200

    Installiere Service Worker - automatische Cache-Invalidierung bei jedem Visit

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_VERSION = 'v' + new Date().toISOString().split('T')[0];
+const CACHE_NAME = `58-tattoo-${CACHE_VERSION}`;
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
